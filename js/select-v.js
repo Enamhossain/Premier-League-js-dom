5fa8f9ec0e630afdf50f7acbca069914a1cfa82e
Enamhossain <bhaianam9@gmail.com>
2022-08-23 19:20:06 +0600

    bouns part add

--- js/select-v.js
+++ js/select-v.js
@@ -8,21 +8,30 @@ function displayPlayer(){
   const children = document.getElementById('playerAll')
   children.textContent = '';
 
-  for(let i = 0; i < playerArray.length; i++)
-    {
-        totalplayer += playerArray[i].playerid;
+ 
 
-        const tr = document.createElement("tr");
-        tr.innerHTML = `
-       
-        <th>${i+1}</th>
-        <td>${playerArray[i].playerid}</td>
-      
-         
-        `;
+    if(playerArray.length > 5){
+        alert('You can add only 5 player Name')
 
-
-        children.appendChild(tr);
+    } else{
+        for(let i = 0; i < playerArray.length; i++)
+        {
+            totalplayer += playerArray[i].playerid;
+    
+            const tr = document.createElement("tr");
+            tr.innerHTML = `
+           
+            <th>${i+1}</th>
+            <td>${playerArray[i].playerid}</td>
+          
+             
+            `;
+    
+    
+            children.appendChild(tr);
+        }
+        return children
+        
     }
 
 }
